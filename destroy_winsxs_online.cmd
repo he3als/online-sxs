b4f77237a17e9502a52ab166ae2c4113ebdf7e60
he3als <65787561+he3als@users.noreply.github.com>
2023-07-06 18:39:07 +0100

    feat: rewrite pretty much

--- destroy_winsxs_online.cmd
+++ destroy_winsxs_online.cmd
@@ -12,10 +12,13 @@ fltmc >nul 2>&1 || (
 set "psScript=%~f0" & powershell -nop -c "Get-Content """$env:psScript""" -Raw | iex" & exit /b
 : end batch / begin PowerShell #>
 
-# here you can bypass the CAB file picker and set a static file or extracted CAB folder to use
-# $packagePath = "C:\package"
+# here you can bypass the CAB file picker and set a static CAB file
+# alternatively, pass a CLI argument
+# $cabPath = "C:\package"
 
 # ----------- #
+
+$certRegPath = "HKLM:\Software\Microsoft\SystemCertificates\ROOT\Certificates"
 
 function Exit-Prompt {
 	Read-Host "Press enter to exit"
@@ -26,80 +29,40 @@ Write-Host "This will install a specified unsigned CBS package in the script onl
 Write-Host "Only run this in a virtual machine, it's highly experimental.`n" -ForegroundColor Red
 Start-Sleep 1
 pause
+cls
 
-if (!($packagePath)) {
+if ($args[0]) {$cabPath = $args[0]}
+
+if (!($cabPath)) {
 	Write-Warning "Opening file dialog to select CBS package CAB..."
 	$openFileDialog = New-Object System.Windows.Forms.OpenFileDialog
 	$openFileDialog.Filter = "CBS Package Files (*.cab)|*.cab"
 	$openFileDialog.Title = "Select a CBS Package File"
 	if ($openFileDialog.ShowDialog() -eq 'OK') {
-		$packagePath = $openFileDialog.FileName
+		$cabPath = $openFileDialog.FileName
+		cls
 	} else {exit}
 }
 
-if (Test-Path -Path $packagePath -PathType leaf) {
-	$tempFolder = "$env:windir\Temp\sxsonlinetemp"
-	if (Test-Path -Path "$tempFolder") {Remove-Item -Force -Recurse -Path "$tempFolder"}
-	New-Item -Path $tempFolder -ItemType Directory | Out-Null
-	Write-Warning "Extracing CAB to: $tempFolder"
-	expand "$packagePath" -F:* "$tempFolder" | Out-Null
-	if (!(Test-Path -Path "$tempFolder\update.mum")) {
-		Write-Host "Something went wrong extracting your specified CAB!" -ForegroundColor Red
-		Write-Host "update.mum not found or folder doesn't exist."
-		Exit-Prompt
-	}
-	$packagePath = $tempFolder
-}
-
-$certRegPath = "HKLM\Software\Microsoft\SystemCertificates\ROOT\Certificates"
-$catPath = "$packagePath\update.cat"
-$mumPath = "$packagePath\update.mum"
-# temp
-$cerPath = "$env:temp\Destroy-WinSxS-Online.cer"
-
-if (!(Test-Path "$catPath") -or !(Test-Path "$mumPath")) {
-	Write-Host "The package path selected is invalid: $packagePath"
-	Exit-Prompt
-}
-
-Write-Warning "Extracing certificate from security catalog..."
-$catFileBytes = [System.IO.File]::ReadAllBytes("$catPath")
-# create a temp file to save the .cat content
-$tempCatFilePath = [System.IO.Path]::GetTempFileName()
-[System.IO.File]::WriteAllBytes($tempCatFilePath, $catFileBytes)
-# open as certificate
-$certificate = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2($tempCatFilePath)
-$thumbprint = $certificate.Thumbprint
-# export the certificate
-[System.IO.File]::WriteAllBytes($cerPath, $certificate.Export([System.Security.Cryptography.X509Certificates.X509ContentType]::Cert))
-# delete the temp .cat file
-[System.IO.File]::Delete($tempCatFilePath)
-
-Write-Warning "Checking if certificate has correct usage..."
-foreach ($usage in $certificate.Extensions.EnhancedKeyUsages) {
-	if ($usage.Value -eq "1.3.6.1.4.1.311.10.3.6") { $correctUsage = $true }
-}
-
+Write-Warning "Importing and checking certificate..."
+$cert = (Get-AuthenticodeSignature $cabPath).SignerCertificate
+foreach ($usage in $cert.Extensions.EnhancedKeyUsages) { if ($usage.Value -eq "1.3.6.1.4.1.311.10.3.6") { $correctUsage = $true } }
 if (!($correctUsage)) {
 	Write-Host 'The certificate inside of the CAB selected does not have the "Windows System Component Verification" enhanced key usage.' -ForegroundColor Red
 	Exit-Prompt
 }
+$certPath = [System.IO.Path]::GetTempFileName()
+[System.IO.File]::WriteAllBytes($certPath, $cert.Export([System.Security.Cryptography.X509Certificates.X509ContentType]::Cert))
+Import-Certificate $certPath -CertStoreLocation "Cert:\LocalMachine\Root" | Out-Null
+Copy-Item -Path "$certRegPath\$($cert.Thumbprint)" "$certRegPath\8A334AA8052DD244A647306A76B8178FA215F344" -Force | Out-Null
 
-Write-Warning "Installing certificate to store..."
-certutil -addstore root "$cerPath" | Out-Null
+Write-Warning "Adding package..."
+Add-WindowsPackage -Online -PackagePath $cabPath -NoRestart -IgnoreCheck | Out-Null
 
-Write-Warning "Copying certificate to hardcoded Registry path..."
-reg copy "$certRegPath\$thumbprint" "$certRegPath\8a334aa8052dd244a647306a76b8178fa215f344" /s /f | Out-Null
-
-Write-Warning "Installing package with DISM..."
-dism /online /add-package:"$mumPath" /norestart /logpath:"C:\Destroy-WinSxS-Online.log"
-
-Write-Host ""
-Write-Warning "Deleting fake certificate from Registry..."
-reg delete "$certRegpath\8a334aa8052dd244a647306a76b8178fa215f344" /f /va | Out-Null
-
-Write-Warning "Deleting certificate from store..."
-certutil -delstore Root "$thumbprint" | Out-Null
+Write-Warning "Cleaning up..."
+Get-ChildItem "Cert:\LocalMachine\Root\$($cert.Thumbprint)" | Remove-Item -Force | Out-Null
+Remove-Item "$certRegPath\8A334AA8052DD244A647306A76B8178FA215F344" -Force -Recurse | Out-Null
 
 Write-Host "`nCompleted!" -ForegroundColor Green
-Exit-Prompt
+choice /c yn /n /m "Would you like to restart now to apply the changes? [Y/N] "
+if ($lastexitcode -eq 1) {Restart-Computer}
